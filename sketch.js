ae5c566f281b5188c7fa151a4c63f3a56510a900
harpreetgill2393 <77834237+harpreetgill2393@users.noreply.github.com>
2021-09-30 14:08:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
-var bow , arrow,  background;
+var bow , arrow,  scene;
 var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
+
+var score=0;
 
 function preload(){
   
@@ -7,6 +9,9 @@ function preload(){
   arrowImage = loadImage("arrow0.png");
   bowImage = loadImage("bow0.png");
   red_balloonImage = loadImage("red_balloon0.png");
+  green_balloonImage = loadImage("green_balloon0.png");
+  pink_balloonImage = loadImage("pink_balloon0.png");
+  blue_balloonImage = loadImage("blue_balloon0.png");
   
 }
 
@@ -25,6 +30,7 @@ function setup() {
   bow.addImage(bowImage); 
   bow.scale = 1;
   
+   score = 0    
 }
 
 function draw() {
@@ -44,17 +50,24 @@ function draw() {
     createArrow();
     
   }
-  
-  //creating continous balloons
-  var select_balloon = Math.round(random(1,1));
+   
+  //creating continous enemies
+  var select_balloon = Math.round(random(1,4));
   
   if (World.frameCount % 100 == 0) {
     if (select_balloon == 1) {
       redBalloon();
+    } else if (select_balloon == 2) {
+      greenBalloon();
+    } else if (select_balloon == 3) {
+      blueBalloon();
+    } else {
+      pinkBalloon();
     }
-  }
-  
+  }  
+    
   drawSprites();
+  text("Score: "+ score, 300,50);
 }
 
 
@@ -69,24 +82,34 @@ function draw() {
   arrow.scale = 0.3;
 }
 
-
 function redBalloon() {
   var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
   red.addImage(red_balloonImage);
   red.velocityX = 3;
   red.lifetime = 150;
   red.scale = 0.1;
-
 }
 
 function blueBalloon() {
-  //write code for spwaning blue balloons
+  var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  blue.addImage(blue_balloonImage);
+  blue.velocityX = 3;
+  blue.lifetime = 150;
+  blue.scale = 0.1;
 }
 
 function greenBalloon() {
-  //write code for spwaning green balloons
+  var green = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  green.addImage(green_balloonImage);
+  green.velocityX = 3;
+  green.lifetime = 150;
+  green.scale = 0.1;
 }
 
 function pinkBalloon() {
-  //write code for spwaning pink balloons
+  var pink = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  pink.addImage(pink_balloonImage);
+  pink.velocityX = 3;
+  pink.lifetime = 150;
+  pink.scale = 1
 }
